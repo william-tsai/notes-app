7c3aa894e0f3a8ff247657d8126788d93fe9b460
William_Tsai <tsaiyuhlin@gmail.com>
2018-01-26 09:59:04 -0800

    Initial Commit

---
+++ belt_examTests/belt_examTests.swift
@@ -0,0 +1,36 @@
+//
+//  belt_examTests.swift
+//  belt_examTests
+//
+//  Created by William Tsai on 1/26/18.
+//  Copyright © 2018 William Tsai. All rights reserved.
+//
+
+import XCTest
+@testable import belt_exam
+
+class belt_examTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
